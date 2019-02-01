0c862e05880ddc8080b39c4dee293b0debc9707a
Bri N <37819830+bn4uc@users.noreply.github.com>
2019-02-01 12:38:52 -0600

    small changes to color

---
+++ theme_colors.R
@@ -0,0 +1,6 @@
+#Brianna Noonan
+
+#for cloropleth maps (red to yellow)
+scale_fill_manual(values = c('#ffffd4', '#fed98e', '#fe9929', '#cc4c02'))
+
+scale_color_manual(values = c('#E8291A','#FFE11A', '#004358', '#1f8a70', '#BEDB39',  '#FD7400', '#BF4182', '#580F45', '#0D177F'))
